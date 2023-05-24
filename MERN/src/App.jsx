5ec4d0d34bc2d6291e5bcb754f7cfec360cbd08f
Jericho Bermas <jecho.deleon@gmail.com>
2023-05-24 14:38:01 +0800

    Added Simple NavBar

--- MERN/src/App.jsx
+++ MERN/src/App.jsx
@@ -1,11 +1,27 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+// Components
+import Home from './pages/Home';
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Header from "./components/Header";
 
-function App (){
-    return (
-        <>
-        <h1 className="mt-5">Tedfsdst</h1>
-        </>
-    );
+function App() {
+  return (
+    <Router>
+      <>
+      <Header></Header>
+        <Routes>
+          
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/signup" element={<Signup />} />
+          <Route exact path="/login" element={<Login />} />
+
+        </Routes>
+      </>
+    </Router>
+  );
 }
 
 export default App;
--- MERN/src/pages/Login.jsx
+++ MERN/src/pages/Login.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div>
+      
+      <h1>hello</h1>
+      
+      </div>
   )
 }
 
