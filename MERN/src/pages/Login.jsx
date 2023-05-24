d4216e8992dfe57bea1a1552d684aa1012e60d2a
Jericho Bermas <jecho.deleon@gmail.com>
2023-05-24 19:59:55 +0800

    Finished Login, Started Signup FrontEnd

--- MERN/src/pages/Login.jsx
+++ MERN/src/pages/Login.jsx
@@ -3,24 +3,103 @@ import { useState } from "react";
 
 //design
 import {
-  TextField,
   InputAdornment,
   IconButton,
   OutlinedInput,
   FormControl,
   InputLabel,
   Button,
+  Input,
+  Box,
+  Container,
+  Typography,
+  Grid,
+  Avatar,
 } from "@mui/material";
+import TextField from "@mui/material/TextField";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+import LockIcon from "@mui/icons-material/Lock";
 
-const [email, setemail] = useState();
 const Login = () => {
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  const avatarStyle = { backgroundColor: "#1bbd7e" };
   return (
-    <div>
-      <h1>Login</h1>
-      <Button variant="outlined">Outlined</Button>
-    </div>
+    <Container maxWidth="sm">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          boxShadow: 3,
+          borderRadius: 2,
+          px: 4,
+          py: 6,
+        }}
+      >
+        <Grid align="center">
+          <Avatar style={avatarStyle}>
+            <LockIcon />
+          </Avatar>
+          <h2>Login</h2>
+        </Grid>
+        <Box component="form" onSubmit={handleSubmit}>
+          {/* Email Form */}
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            label="Email Address"
+            autoComplete="email"
+            autoFocus
+            value={email}
+            onChange={(e) => setemail(e.target.value)}
+          />
+          {/* Password Form */}
+          <FormControl fullWidth required>
+            <InputLabel>Password</InputLabel>
+            <OutlinedInput
+              fullWidth
+              label="Password"
+              type={showPassword ? "text" : "password"}
+              value={password}
+              onChange={(e) => setpassword(e.target.value)}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    edge="end"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                  >
+                    {showPassword ? <VisibilityOffIcon /> : <VisibilityIcon />}
+                  </IconButton>
+                </InputAdornment>
+              }
+            ></OutlinedInput>
+          </FormControl>
+          {/* Button */}
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+            disabled={!email || !password}
+          >
+            Login
+          </Button>
+        </Box>
+      </Box>
+    </Container>
   );
 };
 
--- MERN/src/pages/Signup.jsx
+++ MERN/src/pages/Signup.jsx
@@ -1,9 +1,139 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+
+//design
+import {
+  InputAdornment,
+  IconButton,
+  OutlinedInput,
+  FormControl,
+  InputLabel,
+  Button,
+  Box,
+  Container,
+  Grid,
+  Avatar,
+  FormHelperText,
+} from "@mui/material";
+import TextField from "@mui/material/TextField";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+import LockIcon from "@mui/icons-material/Lock";
+import InputIcon from "@mui/icons-material/Input";
+import AppRegistrationIcon from "@mui/icons-material/AppRegistration";
 
 const Signup = () => {
+  const [Username, setUsername] = useState("");
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
+  const [ConfirmPassword, setConfirmPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  const avatarStyle = { backgroundColor: "#1bbd7e" };
   return (
-    <div>Signup</div>
-  )
-}
+    <Container maxWidth="sm">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          boxShadow: 3,
+          borderRadius: 2,
+          px: 4,
+          py: 6,
+        }}
+      >
+        <Grid align="center">
+          <Avatar style={avatarStyle}>
+            <AppRegistrationIcon />
+          </Avatar>
+          <h2>Signup</h2>
+        </Grid>
+        <Box component="form" onSubmit={handleSubmit}>
+          <TextField
+            autoFocus
+            required
+            margin="normal"
+            fullWidth
+            label="Username"
+            autoComplete="Username"
+            value={Username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          {/* Email Form */}
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            label="Email Address"
+            autoComplete="email"
+            value={email}
+            onChange={(e) => setemail(e.target.value)}
+          />
+          {/* Password Form */}
+          <FormControl fullWidth margin="normal" required>
+            <InputLabel>Password</InputLabel>
+            <OutlinedInput
+              fullWidth
+              required
+              label="Password"
+              type={!showPassword ? "text" : "password"}
+              value={password}
+              onChange={(e) => setpassword(e.target.value)}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    edge="end"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                  >
+                    {showPassword ? <VisibilityOffIcon /> : <VisibilityIcon />}
+                  </IconButton>
+                </InputAdornment>
+              }
+            ></OutlinedInput>
+          </FormControl>
 
-export default Signup
+          <FormControl fullWidth margin="normal">
+            <InputLabel>Confirm Password</InputLabel>
+            <OutlinedInput
+              fullWidth
+              required
+              label="Confirm Password"
+              type={!showPassword ? "text" : "password"}
+              disabled={!password}
+              value={ConfirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            ></OutlinedInput>
+          </FormControl>
+          <FormHelperText>
+            {password === ConfirmPassword ? (
+              <span className="text-success">Password does match!</span>
+            ) : (
+              <span className="text-danger">Password does not match!</span>
+            )}
+          </FormHelperText>
+          {/* Button */}
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+            disabled={!email || !password || !Username || !ConfirmPassword}
+          >
+            Login
+          </Button>
+        </Box>
+      </Box>
+    </Container>
+  );
+};
+
+export default Signup;
