4ce4098d35687ab93e32311fb54bbad3c24f7b53
Jericho Bermas <jecho.deleon@gmail.com>
2023-05-30 19:01:58 +0800

    gfdhxghfshgfx

--- MERN/src/pages/Home.jsx
+++ MERN/src/pages/Home.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
-
+import React, { useContext } from "react";
+import { UserContext } from "../UserContext";
+import Button from "@mui/material/Button";
 const Home = () => {
+  const { user } = useContext(UserContext);
   return (
-    <div>Home</div>
-  )
-}
+    <div className="container text-center" style={{ marginTop: "12rem" }}>
+      3
+      <div className="alert alert-secondary p-5">
+        <h1>{user && <span className="text-success">{user}'s</span>} Home</h1>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
