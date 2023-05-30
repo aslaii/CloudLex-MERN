2caf1502e2a3067d248aff425af4bc6d2e7b7f6a
Jericho Bermas <jecho.deleon@gmail.com>
2023-05-30 09:34:23 +0800

    update 1 file and create 1 file update App.jsx

--- MERN/src/App.jsx
+++ MERN/src/App.jsx
@@ -1,5 +1,10 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import { UserContext } from "./UserContext";
+
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 // Components
 import Home from "./pages/Home";
@@ -7,17 +12,35 @@ import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import Header from "./components/Header";
 
+import { getUser } from "./api/user";
+
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = getUser()
+      .then((res) => {
+        if (res.error) toast(res.error);
+        else setUser(res.username);
+      })
+      .catch((err) => toast(err));
+
+    return () => unsubscribe;
+  }, []);
+
   return (
     <Router>
-      <>
-        <Header></Header>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/signup" element={<Signup />} />
-          <Route exact path="/login" element={<Login />} />
-        </Routes>
-      </>
+      <UserContext.Provider value={{ user, setUser }}>
+        <>
+          <ToastContainer />
+          <Header></Header>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/signup" element={<Signup />} />
+            <Route exact path="/login" element={<Login />} />
+          </Routes>
+        </>
+      </UserContext.Provider>
     </Router>
   );
 }
