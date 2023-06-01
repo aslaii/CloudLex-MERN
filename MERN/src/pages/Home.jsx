a85d8c93d1f939ec066481bfd40760e171a76ca8
jericho Bermas <jecho.deleon@gmail.com>
2023-06-01 23:26:57 +0800

    Added Psuedo Sidebar with Drawer

--- MERN/src/pages/Home.jsx
+++ MERN/src/pages/Home.jsx
@@ -1,16 +1,18 @@
 import React, { useContext } from "react";
 import { Navigate } from "react-router-dom";
 import { UserContext } from "../UserContext";
+import Sidebar from "../components/Sidebar";
 
 const Home = () => {
   const { user } = useContext(UserContext);
   return user ? (
     <>
-      <div className="container text-center" style={{ marginTop: "12rem" }}>
+      {/* <div className="container text-center" style={{ marginTop: "12rem" }}>
         <div className="alert alert-primary p-5">
           <h1>{user && <span className="text-success">{user}'s</span>} Home</h1>
         </div>
-      </div>
+      </div> */}
+      <Sidebar></Sidebar>
     </>
   ) : (
     <Navigate to={"/login"} />
