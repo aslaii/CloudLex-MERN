e2c0d78f049177e07fea10447f6292c514ff8301
echo <jecho.deleon@gmail.com>
2023-06-09 18:04:26 +0800

    Imported Inventory Component

--- MERN/src/pages/Analytics.jsx
+++ MERN/src/pages/Analytics.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Box } from "@mui/material";
+import Inventory from "../components/Inventory";
 const Analytics = () => {
   return (
     <>
       <Box sx={{ ml: 10, mt: -5 }}>
-        <span>Analytics</span>
+        <Inventory />
       </Box>
     </>
   );
