3782dc83322e62cc38e4886b1a58681dc8c07f62
Jericho Bermas <jecho.deleon@gmail.com>
2023-06-11 11:20:46 +0800

    Added inventory schema to he backend

---
+++ Server/models/inventory.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const inventorySchema = new mongoose.Schema({
+  id: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Inventory", inventorySchema);
