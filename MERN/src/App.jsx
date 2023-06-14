bd92201c682aaabb0df916ca3c3100edfcd9a6e9
Jericho Bermas <jecho@aslaii.com>
2023-06-14 17:14:22 +0800

    Added a route to categories page

--- MERN/src/App.jsx
+++ MERN/src/App.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { useNavigate, Navigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 
 import { UserContext } from "./UserContext";
 
@@ -11,9 +11,9 @@ import "react-toastify/dist/ReactToastify.css";
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
-// import Analytics from "./pages/Analytics";
 import Dashboard from "./pages/Dashboard";
 import Inventory from "./pages/Inventory";
+import Categories from "./pages/Categories.jsx";
 //Components
 // import Header from "./components/Header";
 import Appdrawer from "./components/Appdrawer.jsx";
@@ -46,6 +46,7 @@ function App() {
             <Route exact path="/login" element={<Login />} />
             <Route exact path="/dashboard" element={<Dashboard />} />
             <Route exact path="/inventory" element={<Inventory />} />
+            <Route exact path="/categories" element={<Categories />} />
             <Route path="*" element={<Navigate to="/" />} />
           </Routes>
         </>
